a3ed7ea1e64759be02832ca078430f01642599f5
sitahlatif <sitah27@hotmail.com>
2019-03-07 12:38:49 +0300

    task4

--- src/App.js
+++ src/App.js
@@ -6,18 +6,51 @@ import authors from "./data";
 // Components
 import Sidebar from "./Sidebar";
 import AuthorsList from "./AuthorsList";
+import AuthorDetail from "./AuthorDetail";
 
 class App extends Component {
+  state = {
+    currentAuthor: null,
+    filterdAuthors: authors
+  };
+  unselectAuthor = () => {
+    this.setState({ currentAuthor: null });
+  };
+  selectAuthor = author => {
+    const newAuthor = author;
+    this.setState({ currentAuthor: newAuthor });
+  };
+  getContentView = () => {
+    if (this.state.currentAuthor) {
+      return <AuthorDetail author={this.state.currentAuthor} />;
+    } else {
+      return (
+        <AuthorsList
+          authors={this.state.filterdAuthors}
+          selectAuthor={this.selectAuthor}
+          filterAuthors={this.filterAuthors}
+        />
+      );
+    }
+  };
+  //will return True or Falus
+  filterAuthors = query => {
+    this.setState({
+      filterdAuthors: authors.filter(author =>
+        `${author.first_name} ${author.last_name}`
+          .toLowerCase()
+          .includes(query.toLowerCase())
+      )
+    });
+  };
   render() {
     return (
       <div id="app" className="container-fluid">
         <div className="row">
           <div className="col-2">
-            <Sidebar />
+            <Sidebar unselectAuthor={this.unselectAuthor} />
           </div>
-          <div className="content col-10">
-            <AuthorsList authors={authors} />
-          </div>
+          <div className="content col-10">{this.getContentView()}</div>
         </div>
       </div>
     );
--- src/AuthorsList.js
+++ src/AuthorsList.js
@@ -2,15 +2,21 @@ import React, { Component } from "react";
 
 // Components
 import AuthorCard from "./AuthorCard";
+import SearchBar from "./SearchBar";
 
 class AuthorsList extends Component {
   render() {
     const authorCards = this.props.authors.map(author => (
-      <AuthorCard key={author.first_name + author.last_name} author={author} />
+      <AuthorCard
+        key={author.first_name + author.last_name}
+        author={author}
+        selectAuthor={this.props.selectAuthor}
+      />
     ));
 
     return (
       <div className="authors">
+        <SearchBar filterAuthors={this.props.filterAuthors} />
         <h3>Authors</h3>
         <div className="row">{authorCards}</div>
       </div>
